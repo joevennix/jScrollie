61a867cf9c5a5adf9926118b2b0bd0b5aef76486
Joe Vennix <joevennix@gmail.com>
2011-06-29 02:46:58 -0500

    Completed v0.5

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ jScrollie is a lightweight jQuery plugin that allows you to skin your scroll bar
 jScrollie is based almost entirely on http://code.google.com/p/scrollbarpaper/
 
 You can see a demo here: http://joevennix.github.com/jScrollie/
+
+== Version Log ==
+ *  April 26, 2011: v0.1
+ *    - works on latest versions of Safari, Chrome, and FF
+ *    - (timeout?) bug causes scroll to eventually become extremely 
+ *      sluggish on Chrome
+ *  June 29, 2011:  v0.5
+ *    - major code refactor, eliminated need for setTimeout call
+ *    - works on IE8+, Safari 2+, FF3.0+, Chrome 1.0+
+ *    - next release: embedded jScrollie rather than only around <body>
+ *    - problems: no longer supports dynamic resizing (must do manually)
+ *  
+ *  @author Joe Vennix
+ *  http://joevennix.com
--- jquery.jscrollie.js
+++ jquery.jscrollie.js
@@ -1,109 +1,148 @@
 /**
- * Code based off of: http://code.google.com/p/scrollbarpaper/
- * @version last revision April 26 2011
- */
+ *  Code based off of: http://code.google.com/p/scrollbarpaper/
+ *                                                                    
+ *      ,,                                     ,,    ,,    ,,          
+ *      db  .M"""bgd                         `7MM  `7MM    db          
+ *         ,MI    "Y                           MM    MM                
+ *    `7MM `MMb.      ,p6"bo `7Mb,od8 ,pW"Wq.  MM    MM  `7MM  .gP"Ya  
+ *      MM   `YMMNq. 6M'  OO   MM' "'6W'   `Wb MM    MM    MM ,M'   Yb 
+ *      MM .     `MM 8M        MM    8M     M8 MM    MM    MM 8M"""""" 
+ *      MM Mb     dM YM.    ,  MM    YA.   ,A9 MM    MM    MM YM.    , 
+ *      MM P"Ybmmd"   YMbmd' .JMML.   `Ybmd9'.JMML..JMML..JMML.`Mbmmd' 
+ *   QO MP                                                             
+ *   `bmP                                                              
+ *
+ *    jScrollie is a lightweight jQuery plugin that allows you to skin 
+ *  your scroll bars. I wrote it because most of the js scroll bar 
+ *  solutions I had seen felt sluggish and non-native.
+ *    jScrollie works by pushing the original scroll bar behind a div 
+ *  with overflow-x set to hidden, which lets you maintain your original 
+ *  scrolling mechanism. A "fake", HTML-based scroll bar is then generated
+ *  and placed where the user expects the original scroll bar to appear.
+ *  
+ *  == Version Log ==
+ *  April 26, 2011: v0.1
+ *    - works on latest versions of Safari, Chrome, and FF
+ *    - (timeout?) bug causes scroll to eventually become extremely 
+ *      sluggish on Chrome
+ *  June 29, 2011:  v0.5
+ *    - major code refactor, eliminated need for setTimeout call
+ *    - works on IE8+, Safari 2+, FF3.0+, Chrome 1.0+
+ *    - next release: embedded jScrollie rather than only around <body>
+ *    - problems: no longer supports dynamic resizing (must do manually)
+ *  
+ *  @author Joe Vennix
+ *  http://joevennix.com
+ **/
 
-$.fn.extend({
-  jScrollie: function() {
-    this.each(function(i) {
-      var $this = $(this);
-      var jScroll = $this.data('jScrollie');
-      if (jScroll == null) {
-        //initialize the positioning divs
-        var barWidth = 14;
-        $this.wrap('<div class="jScrollieMetaContainer">');
-        $this.before('<div class="jScrollieContainer" style="width:' + barWidth + 'px"><div class="jScrollieTrack"><div class="jScrollieDrag"><div class="jScrollieDragTop"></div><div class="jScrollieDragBottom"></div></div></div></div>');
-        $this.append('<div class="jScrollieClear" style="clear:both;"></div>'); //needed to figure out content height
-        $this.wrap('<div class="jScrollie">');      
-        var scrollie = $this.parent();
-        jScroll = scrollie.prev();
-        var content = $('> :first', $this);
-        //save our settings
-        content.css('overflow', 'hidden');
-        $this.data('jScrollie',      jScroll);
-        $this.data('track',      $('.jScrollieTrack', jScroll));
-        $this.data('drag',       $('.jScrollieDrag', jScroll));
-        $this.data('dragTop',    $('.jScrollieDragTop', jScroll));
-        $this.data('dragBottom', $('.jScrollieDragBottom', jScroll));
-        $this.data('content',    content);
-        $this.data('clearer',    $('> :last', $this));
-        $this.data('scroller',   scrollie);
-        jScroll.hide();
+(function($) {
+  $.jScrollie = function() {
+    // "scrollData" holds variables we need to remember (track, clearer, etc)
+    if ($.jScrollie.scrollData == null) {
+      // initialize jScrollie
+      $body = $('body');
+      // jScrollieClear is used to calculate height of the content
+      var clearer = $('<div class="jScrollieClear" style="clear:both;"></div>')
+                      .appendTo($body);
 
-        var rs=function(e) {
-            //a resize function that should be called when content or window size changes.
-            var offset = scroller.offset();
-            var dataOffset = $this.data('offset');
-            if (($this.height() != $this.data('height'))
-             || (clearer.position().top - content.position().top != $this.data('contentHeight'))
-             || (offset.top != dataOffset.top)
-             || (offset.left != dataOffset.left)) {
-              $this.jScrollie();
-            }
-          }
-        window.setInterval(function() { rs(); }, 400);
-        //other dynamic size cases here? original code had a setInterval loop that resized
+      // jScrollieMetaContainer is a <div> that wraps <body>'s contents.
+      //   It is absolutely positioned to cover the entire window 
+      // jScrollie <div> does the actual 'pushing' of the scroll bar (w=110%)
+      $body.wrapInner('<div class="jScrollieMetaContainer"><div class="jScrollie">');
+      // Insert the following HTML elements just before previous wrap
+      var preWrap = $(
+        '<div class="jScrollieContainer">' + // set WIDTH of bar on this element
+          // the jScrollieContainer sits on right of window, holds "fake" bar
+          '<div class="jScrollieTrack">' + // the vertical bar bg (CSS cust.)
+            '<div class="jScrollieDrag">' + // the draggable "scrollbar"
+              '<div class="jScrollieDragTop">' +    // top "cap" of bar
+              '</div>' +
+              '<div class="jScrollieDragBottom">' + // bottom "cap" of bar
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
+      ).prependTo($body);
+      
+      // remember our initial state
+      $.jScrollie.scrollData = {
+        jScroll    : $('.jScrollieContainer'),
+        track      : $('.jScrollieTrack'),
+        drag       : $('.jScrollieDrag'),
+        dragTop    : $('.jScrollieDragTop'),
+        dragBottom : $('.jScrollieDragBottom'),
+        content    : $('body'),
+        clearer    : clearer,
+        scroller   : $('.jScrollie')
+      };
+      // set up listeners for scroll/click/drag events
+      var checkRefresh = function() {
+        console.log('checking refresh')
+        $.jScrollie.update();
       }
 
-      var barWidth =   $this.data('barWidth');
-      var track =      $this.data('track');
-      var drag =       $this.data('drag');
-      var dragTop =    $this.data('dragTop');
-      var dragBottom = $this.data('dragBottom');
-      var content =    $this.data('content');
-      var clearer =    $this.data('clearer');
-      var scroller = $this.data('scroller');
-      var contentHeight = clearer.position().top - content.position().top;
-      $this.data('height', scroller.height());
-      $this.data('contentHeight', contentHeight);
-      $this.data('offset', scroller.offset());
+      //this.scrollData.scroller.scroll(checkRefresh);
+      $.jScrollie.update();
+    }
+  };
 
-      $this.unbind();
+  $.jScrollie.update = function() {
+    $this = $(this);
+    // called when a scroll, drag, or click is detected that requires a repaint
+    var scrollData = $.jScrollie.scrollData;
+    var contentHeight = scrollData.clearer.position().top - 
+                        scrollData.content.position().top;
+    // save current height/content height
+    scrollData.height = scrollData.scroller.height();
+    scrollData.contentHeight = contentHeight;
+    scrollData.offset = scrollData.scroller.offset();
 
-      //When you click the track, the scrollbar "jumps" to your mouse
-      var ratio = scroller.height() / contentHeight;
+    $this.unbind();
 
-      if (ratio < 1) { //content is big enough to show the scrollbar
-        jScroll.show();
-        content.addClass('jScrollieVisible');
-        jScroll.height(scroller.height()-10);
-        var offset = scroller.offset();
-        jScroll.css('right', '2px').css('top', '10px');
-        var dragHeight = Math.max(Math.round(scroller.height() * ratio), dragTop.height() + dragBottom.height());
-        drag.height(dragHeight-20);
-        var updateDragTop = function() { //called on scroll, and right now
-          drag.css('top', Math.min(Math.round(scroller.scrollTop() * ratio), scroller.height() - dragHeight) + 'px');
-        };
-        updateDragTop();
+    //When you click the track, the scrollbar "jumps" to your mouse
+    var ratio = $.jScrollie.scrollData.scroller.height() / contentHeight;
+    
+    if (ratio < 1) { //content is big enough to show the scrollbar
+      scrollData.jScroll.show();
+      scrollData.jScroll.height(scrollData.scroller.height());
+      var offset = scrollData.scroller.offset();
+      var dragHeight = Math.max( Math.round(scrollData.scroller.height() * ratio), 
+                                 scrollData.dragTop.height() + scrollData.dragBottom.height() );
+      scrollData.drag.height(dragHeight);
 
-        scroller.scroll(function(event) {
-          updateDragTop();
-        });
-
-        var unbindMousemove = function() {
-          $('html').unbind('mousemove.jScrollie');
-        };
-        drag.mousedown(function(event) {
-          unbindMousemove();
-          var offsetTop = event.pageY - drag.offset().top;
-          $('html').bind('mousemove.jScrollie', function(event) {
-            scroller.scrollTop((event.pageY - scroller.offset().top - offsetTop) / ratio);
-            return false;
-          }).mouseup(unbindMousemove);
-          return false;
-        });
-        //When you click the track, the scrollbar "jumps" to your mouse
-        track.mousedown(function(event) {
-          scroller.scrollTop(((event.pageY-20) / scroller.height()) * contentHeight);
-        });
-      }
-      else {
-        //content is small enough to hide the scroll bar
-        $this.unbind();
-        jScroll.hide();
-        content.removeClass('jScrollieVisible');
-        content.width($this.width() - content.innerWidth() + content.width());
-      }
+      // Listen for scorll event
+      scrollData.scroller.scroll(function(event) {
+        $.jScrollie.scrollData.drag.css('top', Math.min(
+                        Math.round($.jScrollie.scrollData.scroller.scrollTop() * ratio), 
+                        $.jScrollie.scrollData.scroller.height() - dragHeight) + 'px'
+                      );
       });
-  }
-});
+      scrollData.scroller.scroll();
+
+      var unbindMousemove = function() {
+        $('html').unbind('mousemove.jScrollie');
+      };
+      // Listen for a "drag" event on the scrollbar
+      scrollData.drag.mousedown(function(event) {
+        unbindMousemove();
+        var drag = scrollData.drag;
+        var offsetTop = event.pageY - drag.offset().top;
+        $('html').bind('mousemove.jScrollie', function(event) {
+
+          scrollData.scroller.scrollTop((event.pageY - scrollData.scroller.offset().top
+                                           - offsetTop) / ratio);
+          return false;
+        }).mouseup(unbindMousemove);
+        return false;
+      });
+      //When you click the track, the scrollbar "jumps" to your mouse
+      scrollData.track.mousedown(function(event) {
+        scrollData.scroller.scrollTop(((event.pageY-20) / scrollData.scroller.height()) * contentHeight);
+      });
+    }
+    else {
+      //content is small enough to hide the scroll bar
+      $.jScrollie.scrollData.jScroll.hide();
+    }
+  };
+})(jQuery);
